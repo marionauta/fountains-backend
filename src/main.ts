e27dfc4be8182895d6fcab55cef5a488cefab8a5
Mario Nachbaur <marionauta@users.noreply.github.com>
2022-11-19 19:54:43 +0100

    feat: implement basic in-memory cache

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { serve } from "https://deno.land/std@0.165.0/http/server.ts";
+import {
+  ConnInfo,
+  Handler,
+  serve,
+} from "https://deno.land/std@0.165.0/http/server.ts";
 import { osmIntoDto } from "./models/drinkingFountainDto.ts";
 import { query as queryArea } from "./osm/nominatim.ts";
-import { query as queryFountains } from "./osm/overpass.ts";
+import { FountainOsm, query as queryFountains } from "./osm/overpass.ts";
 
 const areaName = Deno.env.get("FOUNTAINS_AREA");
 if (!areaName) {
@@ -13,14 +17,28 @@ if (!area) {
   throw "Area not found";
 }
 
-const fountains = await queryFountains(area);
+let lastUpdated: Date | undefined;
+let fountains: FountainOsm[] = [];
+
+const getFountains = async () => {
+  if (
+    lastUpdated === undefined ||
+    new Date().getTime() - lastUpdated.getTime() > (0.5 * 60 * 1000)
+  ) {
+    console.log("Retrieving fountains...");
+    fountains = await queryFountains(area);
+    lastUpdated = new Date();
+  }
+  return fountains;
+};
 
 // http server
 
-const serveFountains = (request: Request): Response => {
+const serveFountains = async (request: Request): Promise<Response> => {
   if (request.method !== "GET") {
     return new Response("Method not allowed", { status: 405 });
   }
+  const fountains = await getFountains();
   const data = fountains.map(osmIntoDto);
   const body = JSON.stringify({ data });
   const headers = {
@@ -29,15 +47,18 @@ const serveFountains = (request: Request): Response => {
   return new Response(body, { status: 200, headers });
 };
 
-const routes: Record<string, (r: Request) => Response> = {
+const routes: Record<string, Handler> = {
   "/v1/drinking-fountains": serveFountains,
 };
 
-const handler = (request: Request): Response => {
+const handler = async (
+  request: Request,
+  connInfo: ConnInfo,
+): Promise<Response> => {
   for (const [pathname, handler] of Object.entries(routes)) {
     const pattern = new URLPattern({ pathname });
     if (pattern.test(request.url)) {
-      return handler(request);
+      return await handler(request, connInfo);
     }
   }
   return new Response("Not found", { status: 404 });
